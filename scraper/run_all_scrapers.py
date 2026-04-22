213b1de36c488908f579248e11bfcc61f11d909b
kanji8210 <denisdekemet@gmail.com>
2026-04-22 22:50:23 +0300

    scraping

--- scraper/run_all_scrapers.py
+++
@@ -1,8 +0,0 @@
-"""
-Run all Mamboleo scrapers for all mapped sources.
-This script is intended for scheduled/automated execution.
-"""
-from main import main
-
-if __name__ == "__main__":
-    main()
